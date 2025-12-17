ca99530b1dda56edda5ec2d5324d39f8060a555e
yujinn00 <qrt365@naver.com>
2025-12-18 01:02:01 +0900

    refactor: MovementRigidbody2D가 PlayerBase의 데이터를 참조하도록 의존성 연결
    1. moveSpeed를 MovementRigidbody2D 컴포넌트의 인스펙터에서 입력하지 않고, PlayerBase의 스탯을 가져오도록 변경

--- Assets/Scripts/Common/MovementRigidbody2D.cs
+++ Assets/Scripts/Common/MovementRigidbody2D.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class MovementRigidbody2D : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
+    private PlayerBase playerBase;
     private Rigidbody2D rigid2D;
 
     private void Awake()
     {
+        playerBase = GetComponent<PlayerBase>();
         rigid2D = GetComponent<Rigidbody2D>();
     }
 
     /// <summary>
-    /// Rigidbody2D를 사용하여 물리적인 이동을 처리하는 함수.
+    /// PlayerBase의 스탯을 참조하여 Rigidbody2D로 물리 이동을 처리하는 함수.
+    /// Rigidbody2D의 선형 속도(linearVelocity)를 직접 설정하여 오브젝트를 이동시킴.
+    /// 방향 벡터(direction)에 이동 속도(moveSpeed)를 곱하여 최종 속도를 결정함.
+    /// Rigidbody를 사용하면 프레임 속도에 관계없이 안정적인 물리 이동이 가능함.
     /// </summary>
     /// <param name="direction">이동하고자 하는 방향 벡터</param>
     public void MoveTo(Vector3 direction)
     {
-        // Rigidbody2D의 선형 속도(linearVelocity)를 직접 설정하여 오브젝트를 이동시킴.
-        // 방향 벡터(direction)에 이동 속도(moveSpeed)를 곱하여 최종 속도를 결정함.
-        // Rigidbody를 사용하면 프레임 속도에 관계없이 안정적인 물리 이동이 가능함.
-        rigid2D.linearVelocity = direction * moveSpeed;
+        // PlayerBase 내부의 PlayerStats에서 실시간 moveSpeed를 가져옴.
+        float currentSpeed = playerBase.Stats.moveSpeed;
+
+        // 가져온 속도를 방향 벡터에 곱하여 물리 속도를 적용함.
+        rigid2D.linearVelocity = direction * currentSpeed;
     }
 }
