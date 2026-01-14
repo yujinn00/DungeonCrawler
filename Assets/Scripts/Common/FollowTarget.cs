089cae1c6933d83433d6847c79c25c994d8b6634
yujinn00 <qrt365@naver.com>
2026-01-14 12:37:54 +0900

    feat: 플레이어가 추적하는 타겟 하단에 시각적 마크 추가

--- Assets/Scripts/Common/FollowTarget.cs
+++ Assets/Scripts/Common/FollowTarget.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
-    [SerializeField]
-    private bool x, y, z;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool x, y, z;
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
 
     private void Update()
     {
@@ -16,9 +20,9 @@ public class FollowTarget : MonoBehaviour
         
         // 활성화된 축은 타겟의 위치를 추적하고, 비활성화된 축은 현재 위치를 유지함.
         transform.position = new Vector3(
-            (x ? target.position.x : transform.position.x),
-            (y ? target.position.y : transform.position.y),
-            (z ? target.position.z : transform.position.z)
+            (x ? target.position.x + offset.x : transform.position.x),
+            (y ? target.position.y + offset.y : transform.position.y),
+            (z ? target.position.z + offset.z : transform.position.z)
         );
     }
 }
--- Assets/Scripts/Player/PlayerBase.cs
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class PlayerBase : EntityBase
 {
+    // 현재 타겟의 위치를 실시간으로 표시하는 조준점 오브젝트.
+    [SerializeField] private FollowTarget targetMark;
+    
     // 플레이어 전용 스탯 데이터.
     [SerializeField] private PlayerStats stats;
 
     // 부모(EntityBase)가 정의한 추상 프로퍼티 구현.
     public override EntityStats Stats => stats;
-    
+
     // 현재 플레이어의 이동 상태.
     public bool IsMoved { get; set; } = false;
 
@@ -16,5 +19,38 @@ public class PlayerBase : EntityBase
         // 플레이어는 몬스터와 달리 별도의 레벨업 공식을 적용하지 않고,
         // 인스펙터에 설정된 초기 스탯을 기반으로 부모의 Setup을 호출하여 초기화함.
         base.Setup();
+    }
+
+    private void Update()
+    {
+        if (Target == null)
+        {
+            targetMark.gameObject.SetActive(false);
+        }
+
+        SearchTarget();
+    }
+
+    private void SearchTarget()
+    {
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var entity in EnemySpawner.Enemies)
+        {
+            // 제일 가까운 적을 찾기 때문에 sqrMagnitude를 사용함.
+            float distance = (entity.transform.position - transform.position).sqrMagnitude;
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                Target = entity.GetComponent<EntityBase>();
+            }
+        }
+
+        if (Target != null)
+        {
+            targetMark.SetTarget(Target.transform);
+            targetMark.transform.position = Target.transform.position;
+            targetMark.gameObject.SetActive(true);
+        }
     }
 }
